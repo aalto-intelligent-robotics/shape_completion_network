6947ea5d3efd7bd7fca4483520ca4c7499e2258a
Jens Lundell <jens.lundell@aalto.fi>
2020-02-28 00:47:17 +0200

    Now download_data.sh pulls the training splits from google drive

--- datasets/download_data.sh
+++ datasets/download_data.sh
@@ -1,3 +1,7 @@
+# Download dataset splits
+wget --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1y5qeEg7Lzxqflup0rET8-VWVJ0cg3xEW' -O datasets.zip 
+unzip -q datasets.zip
+rm datasets.zip
 # Download and unzip ground-truth meshes
 wget --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1SGyNKkptHXaBABnADUiTom_Yt2ghtRMg' -O ground_truth_meshes.zip 
 unzip -q ground_truth_meshes.zip
